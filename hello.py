45b86f9a4aa5b27a248b7fa802ee9c2b37aab63e
BelraQ <bellrara0173@gmail.com>
2023-02-10 21:12:17 +0900

    잘못된 병합

--- hello.py
+++ hello.py
@@ -1,9 +1,3 @@
 print('hello')
-<<<<<<< HEAD
 print('hello world, good bye, see you soon')
 print('test2, hi')
-=======
-print('hello world, good bye, see you again')
-print('test1, hello')
-print('test1, good bye')
->>>>>>> test1
